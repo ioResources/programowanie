e0bd8f4ee0b659a2cfe7ad9999c31a07855e91c6
ioResources <olek.lamza@gmail.com>
2014-02-19 16:30:32 +0100

    Create 01-pierwszy-program.js

---
+++ 01-pierwszy-program.js
@@ -0,0 +1,15 @@
+//deklarujemy zmienną
+var rok;
+
+//pytamy o rok (prompt) i przypisujemy do zmiennej rok
+rok = prompt("Podaj rok urodzenia");
+
+//deklarujemy zmienną aktRok i inicjujemy ją wartością 2014
+var aktRok = 2014;
+
+//deklarujemy kolejną zmienną i przypisujemy do niej wynik odejmowania
+var wiek = aktRok - rok;
+
+//wyświetlamy wynik
+document.write("Masz "+wiek+" lat");
+//alert("Masz "+wiek+" lat");
